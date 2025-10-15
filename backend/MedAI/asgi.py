da4aa0340b3e3311b46ceb8426b418d2ccea9f0b
Mostafa Atef <mostafaatefsaad705@gmail.com>
2025-10-15 03:14:16 +0300

    remove the unnecessary package python-decouple and use django-configurations for better configuration management

--- backend/MedAI/asgi.py
+++ backend/MedAI/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from configurations.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MedAI.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
+
 
 application = get_asgi_application()
