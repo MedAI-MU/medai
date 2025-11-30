d6216ff2c8cd86cc7dde54f9babd2b96a87e9635
mena <mena.rizkalla1@gmail.com>
2025-11-30 04:01:53 +0200

    feat(build): Add kotlinx-datetime dependency
    *   build: Add `kotlinx-datetime` version 0.6.0 to `libs.versions.toml`.
    *   build: Implement `kotlinx.datetime` library in `composeApp/build.gradle.kts`.

--- mobile/MedAI/composeApp/build.gradle.kts
+++ mobile/MedAI/composeApp/build.gradle.kts
@@ -47,6 +47,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(compose.materialIconsExtended)
+            implementation(libs.kotlinx.datetime)
 
             // Koin
             implementation(libs.koin.core)
