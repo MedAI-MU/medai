b320d032d74e72944ca879d4890770d38458baf1
mark <mark.rizkalla1@gmail.com>
2025-12-02 08:24:31 +0200

    feat: Add navigation events for login screen
    *   Add `ForgotPasswordClicked` and `SignUpClicked` to `LoginEvent`.
    *   Introduce `LoginEffect` sealed interface to handle navigation and side effects.
    *   Define `NavigateToHome`, `NavigateToForgotPassword`, `NavigateToSignUp`, and `ShowError` effects.

--- mobile/MedAI/composeApp/src/commonMain/kotlin/org/example/project/presentation/loginScreen/LoginEvent.kt
+++ mobile/MedAI/composeApp/src/commonMain/kotlin/org/example/project/presentation/loginScreen/LoginEvent.kt
@@ -5,4 +5,14 @@ sealed class LoginEvent {
     data class PasswordChanged(val value: String) : LoginEvent()
     object LoginClicked : LoginEvent()
     object ErrorShown : LoginEvent() // Reset error after showing snackbar
+    object ForgotPasswordClicked : LoginEvent()
+    object SignUpClicked : LoginEvent()
+
+}
+
+sealed interface LoginEffect {
+    object NavigateToHome : LoginEffect
+    object NavigateToForgotPassword : LoginEffect //Navigates to SetPasswordScreen
+    data class ShowError(val message: String) : LoginEffect
+    object NavigateToSignUp : LoginEffect
 }
