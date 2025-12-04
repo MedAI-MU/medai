e55633178efb33ef3b596daa95612c97ac645203
Mostafa Atef <mostafaatefsaad705@gmail.com>
2025-12-04 14:17:16 +0200

    add unit and e2e tests for user module

---
+++ backend/src/users/users.service.spec.ts
@@ -0,0 +1,79 @@
+jest.mock('argon2', () => ({
+  hash: jest.fn(),
+}));
+
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+import { Test } from '@nestjs/testing';
+import { RegisterDto } from './dtos/register.dto';
+import * as argon2 from 'argon2';
+import { ConflictException } from '@nestjs/common';
+
+describe('users.service', () => {
+  let usersService: UsersService;
+
+  const usersRepositoryMock = {
+    save: jest.fn(),
+    findOne: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: usersRepositoryMock,
+        },
+      ],
+    }).compile();
+
+    usersService = module.get<UsersService>(UsersService);
+
+    usersRepositoryMock.findOne.mockReset();
+    usersRepositoryMock.save.mockReset();
+    (argon2.hash as jest.Mock).mockReset();
+  });
+
+  it('should be defined', () => {
+    expect(usersService).toBeDefined();
+  });
+
+  describe('registerUser', () => {
+    it('should hash the password, register the user and set id', async () => {
+      usersRepositoryMock.findOne.mockResolvedValue(null);
+      (argon2.hash as jest.Mock).mockResolvedValue('hashedpassword');
+      usersRepositoryMock.save.mockImplementation((user: User) => {
+        user.id = 1;
+        return user;
+      });
+      const dto: RegisterDto = {
+        name: 'Test User',
+        email: 'test@test.com',
+        password: 'strongpassword',
+        phone: '00000000000',
+      };
+
+      const result = await usersService.registerUser(dto);
+
+      expect(argon2.hash).toHaveBeenCalledWith(dto.password);
+      expect(result.id).toBe(1);
+      expect(result.password).toBe('hashedpassword');
+    });
+
+    it('should throw a conflict error if email is already in use', async () => {
+      usersRepositoryMock.findOne.mockResolvedValue({ id: 1 } as User);
+      const dto: RegisterDto = {
+        name: 'Test User',
+        email: 'test@test.com',
+        password: 'strongpassword',
+        phone: '00000000000',
+      };
+
+      await expect(usersService.registerUser(dto)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+  });
+});
---
+++ backend/tests/users.e2e-spec.ts
@@ -0,0 +1,120 @@
+import request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
+import { RefreshToken } from 'src/users/entities/refresh-token.entity';
+import { RegisterDto } from 'src/users/dtos/register.dto';
+
+describe('UsersController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        UsersModule,
+        TypeOrmModule.forRoot({
+          type: 'sqljs',
+          autoSave: false,
+          location: ':memory:',
+          synchronize: true,
+          dropSchema: true,
+          entities: [User, RefreshToken],
+        }),
+      ],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+    await app.init();
+  });
+
+  describe('/users/register (POST)', () => {
+    const REGISTER_USER_URL = '/users/register';
+    const invalidRegisterDtotestCases = [
+      {
+        name: 'a',
+        email: 'test@test.com',
+        password: '123456',
+        phone: '01012345678',
+      },
+      {
+        name: 'Valid Name',
+        email: 'invalidemail',
+        password: '123456',
+        phone: '01012345678',
+      },
+      {
+        name: 'Valid Name',
+        email: 'test@test.com',
+        password: '123',
+        phone: '01012345678',
+      },
+      {
+        name: 'Valid Name',
+        email: 'test@test.com',
+        password: '123456',
+        phone: '1234567890',
+      },
+      { name: '', email: 'invalid', password: '123', phone: '000' },
+      {
+        name: 'Valid Name',
+        email: 'test@test.com',
+        password: '123456',
+        phone: '01012345678',
+        extraField: 'notallowed',
+      },
+    ];
+
+    it.each(invalidRegisterDtotestCases)(
+      'should return 400 for invalid register dto: %o',
+      async (invalidDto) => {
+        await request(app.getHttpServer())
+          .post(REGISTER_USER_URL)
+          .send(invalidDto)
+          .expect(400);
+      },
+    );
+
+    it('should register a user and return 201 status', async () => {
+      const dto: RegisterDto = {
+        name: 'Valid Name',
+        email: 'test@test.com',
+        password: '123456',
+        phone: '01012345678',
+      };
+      await request(app.getHttpServer())
+        .post(REGISTER_USER_URL)
+        .send(dto)
+        .expect(201);
+    });
+
+    it('should return 409 if email is already in use', async () => {
+      const duplicateDto: RegisterDto = {
+        name: 'Test Name',
+        email: 'test2@test.com',
+        password: '123456',
+        phone: '01012345678',
+      };
+      await request(app.getHttpServer())
+        .post(REGISTER_USER_URL)
+        .send(duplicateDto);
+
+      await request(app.getHttpServer())
+        .post(REGISTER_USER_URL)
+        .send(duplicateDto)
+        .expect(409);
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
