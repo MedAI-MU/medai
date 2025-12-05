11c0bdf52c7b0812aeaeff4093f78939fb9e8363
Mostafa Atef <mostafaatefsaad705@gmail.com>
2025-12-05 19:31:25 +0200

    enhance validation rules on dtos and use validation pipe in LocalAuthGuard canActivate method to validate login dto

--- backend/src/auth/guards/local-auth.guard.ts
+++ backend/src/auth/guards/local-auth.guard.ts
@@ -2,11 +2,48 @@ import {
   BadRequestException,
   ExecutionContext,
   Injectable,
+  ValidationPipe,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { LoginDto } from '../dto/login.dto';
+import { Request } from 'express';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
+  private readonly validationPipe = new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+    exceptionFactory: (errors) => {
+      const messages = errors.map((err) => ({
+        field: err.property,
+        errors: Object.values(err.constraints!),
+      }));
+
+      return new BadRequestException({
+        success: false,
+        message: 'Validation failed',
+        validationErrors: messages,
+      });
+    },
+  });
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      // Validate the body first and assign a properly typed body
+      const req = context
+        .switchToHttp()
+        .getRequest<Request & { body: LoginDto }>();
+      const validated = (await this.validationPipe.transform(req.body, {
+        type: 'body',
+        metatype: LoginDto,
+      })) as LoginDto;
+      req.body = validated;
+    } catch (err) {
+      throw new BadRequestException(err);
+    }
+    return super.canActivate(context) as boolean | Promise<boolean>;
+  }
   handleRequest<TUser = any>(
     err: any,
     user: any,
