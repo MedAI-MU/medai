0fc7ecd62424de0c4896d9827e4dd317d6a650d2
Mostafa Atef <37014005+MustafaAtef@users.noreply.github.com>
2026-05-04 14:51:14 +0300

    Staging Workflow (#78)
    * add the initial azure deployment workflow with build and push to acr
    
    * fix the encryption config of database connection
    
    * add deploy job to the workflow
    
    * add the migration steps in the deploy job
    
    * refactor the build and push job to use github actions cache to speed up the build process
    
    * refactor the build and push job to use github actions cache to speed up the build process

--- backend/typeorm.data-source.ts
+++ backend/typeorm.data-source.ts
@@ -6,11 +6,15 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: configService.getOrThrow<string>('DB_HOST'),
   port: parseInt(configService.getOrThrow<string>('DB_PORT')),
   username: configService.getOrThrow<string>('DB_USERNAME'),
   password: configService.getOrThrow<string>('DB_PASSWORD'),
   database: configService.getOrThrow<string>('DB_NAME'),
   migrations: ['./src/database/migrations/**/*{.js,.ts}'],
   entities: ['./src/**/entities/*{.js,.ts}'],
+  ssl: { rejectUnauthorized: false },
+  extra: {
+    ssl: { rejectUnauthorized: false },
+  },
 });
