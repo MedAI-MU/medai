1687ca337d3292d0c509955880f7b09643008971
Abdalrahman_Gamal <abdalrahmangamal200@gmail.com>
2026-05-06 05:36:21 +0300

    feat(front): added all-doctors page for the patient and enabled user to search by either name or speciality

--- frontend-next/src/components/ui/FormInput.js
+++ frontend-next/src/components/ui/FormInput.js
@@ -1,16 +1,18 @@
 import ErrorMessage from "./ErrorMessage";
 
-function FormInput({ label, error, startIcon, endIcon, ...attrs }) {
+function FormInput({ label, error, startIcon, endIcon, containerClassName, ...attrs }) {
   let px = "";
   if (startIcon) px += "ps-10";
   if (endIcon) px += " pe-12";
   if (!startIcon && !endIcon) px = "px-4";
 
   return (
-    <div>
-      <label className="text-text-base mb-2 block text-sm font-medium">
-        {label}
-      </label>
+    <div className={containerClassName}>
+      {label && (
+        <label className="text-text-base mb-2 block text-sm font-medium">
+          {label}
+        </label>
+      )}
       <div className="relative">
         {startIcon && (
           <span className="text-text-subtle absolute top-1/2 left-3 flex h-5 w-5 -translate-y-1/2 items-center justify-center">
