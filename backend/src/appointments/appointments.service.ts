af7479ebe00146c6d644ffd780bcc385fe951868
Ahmed Gouda <ahmed.gouda@owasp.org>
2026-06-28 19:51:21 +0300

    Delete appointment after cancellation

--- backend/src/appointments/appointments.service.ts
+++ backend/src/appointments/appointments.service.ts
@@ -153,12 +153,12 @@ export class AppointmentsService {
       }
     }
 
-    if (
-      dto.status === AppointmentStatusEnum.CANCELLED &&
-      appointment.status !== 'cancelled'
-    ) {
+    if (dto.status === AppointmentStatusEnum.CANCELLED) {
       appointment.scheduleSlot.status = 'available';
       await this.slotsRepository.save(appointment.scheduleSlot);
+      await this.appointmentsRepository.remove(appointment);
+      appointment.status = AppointmentStatusEnum.CANCELLED;
+      return appointment;
     }
 
     if (
