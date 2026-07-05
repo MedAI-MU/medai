6029c15c168d67ee5bccc5e77c85325ec32e1f1d
Mostafa Atef <37014005+MustafaAtef@users.noreply.github.com>
2026-07-05 18:39:37 +0300

    feat(terraform): create app database on PostgreSQL flexible server (#205)

--- infra/modules/infrastructure/main.tf
+++ infra/modules/infrastructure/main.tf
@@ -163,6 +163,13 @@ resource "azurerm_postgresql_flexible_server" "db" {
   }
 }
 
+resource "azurerm_postgresql_flexible_server_database" "app" {
+  name      = var.db_name
+  server_id = azurerm_postgresql_flexible_server.db.id
+  charset   = "UTF8"
+  collation = "C.UTF-8"
+}
+
 resource "azurerm_postgresql_flexible_server_firewall_rule" "client_ip" {
   name             = "client-ip-address"
   server_id        = azurerm_postgresql_flexible_server.db.id
