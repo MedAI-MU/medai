acf137d53adf932b7e68f2ac43abf30621b5e875
Abdalrahman_Gamal <abdalrahmangamal200@gmail.com>
2026-07-09 21:55:52 +0300

    feat: added default user avatar

--- frontend-next/src/components/ui/UserInfo.js
+++ frontend-next/src/components/ui/UserInfo.js
@@ -1,5 +1,7 @@
 import { getUser } from "@/services/server/auth";
 import ErrorMessage from "./ErrorMessage";
+import defaultAvatar from "@/assets/default-user.jpg";
+import Image from "next/image";
 
 async function UserInfo() {
   let user;
@@ -22,13 +24,10 @@ async function UserInfo() {
 
       {/* Avatar */}
       <div className="border-surface bg-primary/20 size-9 shrink-0 overflow-hidden rounded-full border-2 md:size-10">
-        <img
+        <Image
           alt="User avatar"
           className="h-full w-full object-cover"
-          src={
-            user?.avatar ||
-            "https://lh3.googleusercontent.com/aida-public/AB6AXuAOcWE1M30lFGq-GjI6ngcTxkRqQomwggB3oNi-WAZwrzRGkM02YAQEBShWWHPvoAYSzlyLA7a96eeDZlqr5D7pLCRXdvR3dbk8RcLZvIrgQ4iTy4lEt-tLgHaMfhTmqk4C4o-wEXMINTefQgYrCRFfHEvbHY6EHwqYR_S4cDg-zGkgF9grT_ds4MBR1jlKH3Nrv_ZI0N1yjGm5dJLqO_GlbVfECJO-4M8dV136Jq06XDCyjwyxIsUllUCeBNiSGI2yCi-qXDC0UoM"
-          }
+          src={user?.avatar || defaultAvatar}
         />
       </div>
     </div>
