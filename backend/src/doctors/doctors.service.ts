4c04c2ff8714942b7f0dd3d986036265f242fd57
Ahmed Gouda <ahmed.gouda@owasp.org>
2026-07-11 12:33:22 +0300

    Add tests

--- backend/src/doctors/doctors.service.ts
+++ backend/src/doctors/doctors.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Doctor } from './entities/doctor.entity';
 import { ILike, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
